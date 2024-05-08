7a2aa1fb16709691913f8779ccb6c4eabe26a4dc
FP03 <142733625+FP03@users.noreply.github.com>
2024-05-08 20:11:07 +0800

    Delete Project directory

--- Project/trylogin/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("textInput").addEventListener("focus", function () {
-  document.getElementById("envelopeIcon").style.color = "lightblue";
-});
-
-document.getElementById("textInput").addEventListener("blur", function () {
-  document.getElementById("envelopeIcon").style.color = "#000"; // Change back to initial color when focus is lost
-});
